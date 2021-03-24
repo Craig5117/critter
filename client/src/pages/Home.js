bb2a27234d558b83e95168d5791880c31cc646ca
Craig5117 <craig.bennett5117@gmail.com>
2021-03-24 19:23:53 -0400

    fixed date formatter

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ProfileImage from '../components/ProfileImage';
-import PetCard from '../components/PetCard'
+import PetCard from '../components/PetCard';
+import TailList from '../components/TailList';
 
-function Home () {
-    const dbPets = [
-        
-    ]
+function Home() {
+  const dbPets = [];
+  const dbTails = [
+    {
+      tailText: 'Slept on the couch all day. My human keeps running the vacuum. Humans are so weird.',
+      createdAt: 'Mar 13th, 2021 at 3:20 am',
+      petUsername: 'FluffyEars99',
+      commentCount: 34
+    },
+  ];
 
-    return (
-        <div>
-            <div>This is the Home Page. Maybe we should put the latest Posts here?</div>
-            <ProfileImage />
-            <PetCard />
-        </div>
-        
-    )
+  return (
+    <div>
+      <div>
+        This is the Home Page. Maybe we should put the latest Posts here?
+      </div>
+      <ProfileImage />
+      <PetCard />
+      <TailList />
+    </div>
+  );
 }
 
 export default Home;
