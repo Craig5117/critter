87fb9722b811fd59547c97a5903d38004e2b9b11
Craig5117 <craig.bennett5117@gmail.com>
2021-03-23 20:04:45 -0400

    added ProfileImage

---
+++ client/src/components/ProfileImage/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Image from 'react-bootstrap';
+
+function ProfileImage () {
+    const [image, setImage] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const uploadImage = async (e) => {
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'critter');
+        setLoading(true)
+        const res = await fetch (
+            '	https://api.cloudinary.com/v1_1/critter-cloud/image/upload',
+            {
+                method: 'POST',
+                body: data
+            }
+        )
+        const file = await res.json();
+        setImage(file.secure_url)
+        setLoading(false)
+    }
+    return (
+        <div>
+        <h3 id="upload-heading">Upload Image</h3>
+       <input aria-labelledby="upload-heading" name="file" type="file" placeholder="Upload an image" onChange={uploadImage} />
+        {loading ? (
+            <h4>loading</h4>
+        ): (<Image src={image} style={{width: '300px'}} rounded></Image>)
+        }
+        </div>
+    )
+}
+
+export default ProfileImage;
