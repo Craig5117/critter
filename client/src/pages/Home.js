984ff1dcdf2c287b93b22606eb00500fd3a25fdf
Craig5117 <craig.bennett5117@gmail.com>
2021-03-23 20:07:25 -0400

    imported Profile image to Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import ProfileImage from '../components/ProfileImage';
+
 
 function Home () {
     return (
-        <div>This is the Home Page. Maybe we should put the latest Posts here?</div>
+        <div>
+            <div>This is the Home Page. Maybe we should put the latest Posts here?</div>
+            <ProfileImage />
+        </div>
+        
     )
 }
 
