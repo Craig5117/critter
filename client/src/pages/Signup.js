e3acd3cfb85c1c4ad32596d0d71c4cf901ae9832
Craig5117 <craig.bennett5117@gmail.com>
2021-03-26 17:11:50 -0400

    started setting up signup layout and renamed SinglePost to SingleTail

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -7,7 +7,43 @@ import Auth from '../utils/auth';
 
 function Signup () {
     return(
-        <p>We will set up our signup form here, using React Bootstrap's Form component</p>
+        <Form>
+  <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Enter your email address</Form.Label>
+    <Form.Control type="email" placeholder="name@example.com" />
+  </Form.Group>
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Enter a password that is at least 8 characters</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>Example select</Form.Label>
+    <Form.Control as="select">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </Form.Control>
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect2">
+    <Form.Label>Example multiple select</Form.Label>
+    <Form.Control as="select" multiple>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </Form.Control>
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Example textarea</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
     ) 
 }
 
