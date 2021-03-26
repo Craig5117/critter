c2ccb3b03a41b7835d7c55f2784e97ec198370b7
Craig5117 <craig.bennett5117@gmail.com>
2021-03-25 21:01:23 -0400

    added queries to front-end

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,33 @@
 import gql from 'graphql-tag';
 
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      petType
+      age
+      sex
+      bio
+      image
+      relationshipStatus
+      friends {
+        _id
+        username
+      }
+      tails {
+        _id
+        tailText
+        createdAt
+        commentCount
+      }
+    }
+  }
+`;
+
 export const QUERY_TAILS = gql`
-query tails($username: String) {
+  query tails($username: String) {
     tails(username: $username) {
       _id
       tailText
@@ -19,7 +45,7 @@ query tails($username: String) {
 `;
 
 export const QUERY_TAIL = gql`
-    query tail($id: ID!) {
+  query tail($id: ID!) {
     tail(_id: $id) {
       _id
       tailText
@@ -37,4 +63,68 @@ export const QUERY_TAIL = gql`
 `;
 
 export const QUERY_PETS = gql`
-`
+  query {
+    pets {
+      _id
+      username
+      email
+      petType
+      age
+      sex
+      bio
+      image
+      relationshipStatus
+      friends {
+        _id
+        username
+      }
+      tails {
+        _id
+        tailText
+        createdAt
+        commentCount
+      }
+    }
+  }
+`;
+
+export const QUERY_PETS_BASIC = gql`
+  query {
+    pets {
+      _id
+      username
+      petType
+      age
+      sex
+      bio
+      image
+      relationshipStatus
+    }
+  }
+`;
+
+export const QUERY_PET = gql`
+  query pet($username: String!) {
+    pet(username: $username) {
+      _id
+      username
+      email
+      petType
+      age
+      sex
+      bio
+      image
+      relationshipStatus
+      friends {
+        _id
+        username
+      }
+      tails {
+        _id
+        tailText
+        createdAt
+        commentCount
+      }
+    }
+  }
+`;
