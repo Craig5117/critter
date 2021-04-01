4bedbc3c19f5a4d07bade32c503c8e8f2cdaa01a
Adam Crandall <adamcrandall91@gmail.com>
2021-04-01 10:16:38 -0400

    Models index.js re-added

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Pet = require('./Pet');
+const Tail = require('./Tail');
+const PetType = require('./PetType');
+const commentSchema = require('./Comment');
+
+module.exports = { Pet, Tail, PetType, commentSchema };
