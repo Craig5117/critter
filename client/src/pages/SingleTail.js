beb8c03ff6c085f5e6333e8c25e97c944e7948cb
Craig5117 <craig.bennett5117@gmail.com>
2021-04-01 12:57:04 -0400

    added offline rendering of single tail page

--- client/src/pages/SingleTail.js
+++ client/src/pages/SingleTail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_TAIL } from '../utils/queries';
@@ -8,17 +8,31 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import CommentList from '../components/CommentList'
 import Auth from '../utils/auth';
+import { idbPromise } from '../utils/helpers';
 import { Link } from 'react-router-dom';
 import './SingleTail.css'
 
 function SingleTail(props){
     const { id: tailId } = useParams();
+    const [tail, setTail] = useState({})
 
     const { loading, data } = useQuery(QUERY_TAIL, {
         variables: { id: tailId }
         });
 
-    const tail = data?.tail || {};
+    // const tail = data?.tail || {};
+
+    useEffect(() => {
+      if (data?.tail) {
+        setTail(data.tail)
+      } else if (!loading) {
+        idbPromise('tails', 'get').then((tailData) => {
+          const currentTail = tailData.filter((tail) => tail._id === tailId);
+          setTail(currentTail[0]);
+        });
+      }
+      console.log(tail)
+    }, [data, loading, tailId, tail])
 
     if (loading) {
         return <div>Loading...</div>;
@@ -26,11 +40,13 @@ function SingleTail(props){
 
     return (
         <div>
+          {!loading &&
             <Card>
                 
                 <div className="d-flex flex-wrap px-4">
                   <Col xs={12} md={4} lg={3}>
                     <div className="user-card">
+                    {tail?.postedBy && 
                     <Link className="p-0 tail-link" to={`/profile/${tail.postedBy._id}`}>
                      <Card.Footer className="d-flex tail-card mt-2">
                         <Col className="">
@@ -43,7 +59,7 @@ function SingleTail(props){
                         src={tail.postedBy.image}
                         
                       />
-                    </Link>
+                    </Link>}
                     </div>
                   </Col>
                   <Col xs={12} md={8} lg={9}>
@@ -57,7 +73,7 @@ function SingleTail(props){
                 {Auth.loggedIn() &&  <CommentForm tailId={tail._id} />}
               </Card>
             
-         
+          }
         </div>
     );
 }
